3796f6bd0d84ca133023a1ee8c35a9288bfeaedf
Pranav <sankpalpranav022@gmail.com>
2026-07-01 11:30:00 +0530

    Added Interface

--- Stock-Price-Predictor/Interface.py
+++ Stock-Price-Predictor/Interface.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
+st.title("Stock Price Predictor")
 
-st.write("Hello World")
+st.divider()
+st.write("Making stock price predictions using Machine Learning algorithms like **Linear Regression, Random Forest and Ensemble Approach**."
+             "This project is in its initial state and going to add Deep learning algorithms like LSTM and NLP for better understanding of Fundamental News and market sentiment")
+st.divider()
