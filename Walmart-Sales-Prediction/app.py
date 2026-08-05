e381ebaf5c2b50bf61bbba2435d5578a08967bfc
Pranav <sankpalpranav022@gmail.com>
2026-08-06 01:15:02 +0530

    Fix: Fixed deployment errors

--- Walmart-Sales-Prediction/app.py
+++ Walmart-Sales-Prediction/app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 # Load model
 @st.cache_resource
 def load_model():
-    return joblib.load("model/XGBoost_model.pkl")
+    return joblib.load("./model/XGBoost_model.pkl")
 
 model = load_model()
 
